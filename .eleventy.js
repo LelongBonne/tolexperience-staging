03a23dcfc2f204235d23eacdaabf5d4fec87bdbf
LelongBonne <lelong.bonne@gmail.com>
2025-08-13 16:23:19 +0200

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+export default function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
+  eleventyConfig.addPassthroughCopy({"src/robots.txt": "robots.txt"});
+  return {
+    dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+}
