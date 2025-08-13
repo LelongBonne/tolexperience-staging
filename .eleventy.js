dccab21543e5590647150f42b1fe07fb9b3ae5bd
LelongBonne <lelong.bonne@gmail.com>
2025-08-13 19:36:04 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,9 @@
-/** .eleventy.js */
 module.exports = function (eleventyConfig) {
-  // (facultatif) copier des assets si besoin
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-
   return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site",
-    },
-    // Très important : on rend les pages HTML et Markdown avec Nunjucks
+    dir: { input: "src", includes: "_includes", output: "_site" },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    // Et on précise qu'on veut bien traiter .njk et .md
     templateFormats: ["njk", "md", "html"]
   };
 };
