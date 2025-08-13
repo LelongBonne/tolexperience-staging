f8bd525d3e1fe4dcecc03db116d60327ba008fb2
LelongBonne <lelong.bonne@gmail.com>
2025-08-13 18:14:24 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
   eleventyConfig.addPassthroughCopy({"src/robots.txt": "robots.txt"});
+
+  // ➜ Collections
+  eleventyConfig.addCollection("cours", c => c.getFilteredByGlob("src/cours/**/index.md"));
+  eleventyConfig.addCollection("industrie", c => c.getFilteredByGlob("src/cours/industrie/**/index.md"));
+  eleventyConfig.addCollection("education", c => c.getFilteredByGlob("src/cours/education/**/index.md"));
+
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
     markdownTemplateEngine: "njk",
