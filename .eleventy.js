fd5f3f58ecda61a52954a540d514d5e0c8104acb
LelongBonne <lelong.bonne@gmail.com>
2025-08-15 16:00:04 +0200

    fix: eleventy layout + liens absolus + assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  
   return {
-    dir: { input: "src", includes: "_includes", output: "_site" },
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    templateFormats: ["njk", "md", "html"],
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
-    templateFormats: ["njk", "md", "html"]
   };
 };
